68192b46ffca42139de7e3820c655b55d256f514
Jesé Nehemías Alvarado Salvador <jalvarado@univo.edu.sv>
2021-11-26 22:57:24 -0600

    Add Entities layer

--- excercises/001-ncapas/NWind/DAL/NWindDBModel.Context.cs
+++ excercises/001-ncapas/NWind/DAL/NWindDBModel.Context.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DAL
+namespace Entities
 {
     using System;
     using System.Data.Entity;
