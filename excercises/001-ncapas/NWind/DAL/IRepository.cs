6eb44ee5d5becf37090986c8792d4f3c2341f6a0
Jesé Nehemías Alvarado Salvador <jalvarado@univo.edu.sv>
2021-12-01 08:46:51 -0600

    Add IRepository  crud methods

--- excercises/001-ncapas/NWind/DAL/IRepository.cs
+++ excercises/001-ncapas/NWind/DAL/IRepository.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    interface IRepository
+    public interface IRepository : IDisposable
     {
+        // Para agregar nueva entidad a la BD
+        TEntity Create<TEntity>(TEntity toCreate) where TEntity : class;
+
+        // Para eliminar una entidad
+        bool Delete<TEntity>(TEntity toDelete) where TEntity : class;
+
+        // Para actualizar una entidad
+        bool Update<TEntity>(TEntity toUpdate) where TEntity : class;
+
+        TEntity Retrieve<TEntity>(Expression<Func<TEntity, bool>> criteria)
+            where TEntity : class;
+
+        // Para recuperar un conjunto de entidades que cumplan con un criterio de búsqueda
+        List<TEntity> Filter<TEntity>(Expression<Func<TEntity, bool>> criteria)
+            where TEntity : class;
     }
 }
